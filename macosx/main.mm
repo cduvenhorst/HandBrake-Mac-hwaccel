cf4e4321a5008d9272ff322f347686fda875a501
cduvenhorst <cduvenhorst@nospam.please>
2013-10-19 18:00:32 +0200

    Deleted unused function and eliminated a warning of a missing prototype definition.

--- macosx/main.mm
+++ macosx/main.mm
@@ -11,7 +11,7 @@
 
 #import "hb.h"
 
-void SigHandler( int signal )
+static void SigHandler( int signal )
 {
     [NSApp terminate: NULL];
 } 
@@ -23,44 +23,9 @@ void SigHandler( int signal )
  * this is the only place I could find that looked like C :)
 ****************************************************************************/
 extern "C" {
-    void hb_error_handler( const char *errmsg )
+    static void hb_error_handler( const char *errmsg )
     {
          fprintf(stderr, "GUI ERROR dialog: %s\n", errmsg );
-    }
-}
-
-char * str_printf(const char *fmt, ...)
-{
-    /* Guess we need no more than 100 bytes. */
-    int len;
-    va_list ap;
-    int size = 100;
-    char *tmp, *str = NULL;
-
-    str = (char*)malloc(size);
-    while (1) 
-    {
-        /* Try to print in the allocated space. */
-        va_start(ap, fmt);
-        len = vsnprintf(str, size, fmt, ap);
-        va_end(ap);
-
-        /* If that worked, return the string. */
-        if (len > -1 && len < size) {
-            return str;
-        }
-
-        /* Else try again with more space. */
-        if (len > -1)    /* glibc 2.1 */
-            size = len+1; /* precisely what is needed */
-        else           /* glibc 2.0 */
-            size *= 2;  /* twice the old size */
-
-        tmp = (char*)realloc(str, size);
-        if (tmp == NULL) {
-            return str;
-        }
-        str = tmp;
     }
 }
 
